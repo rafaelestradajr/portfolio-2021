6c87299c1661b52af6fcd52279e92d0b43a62724
Rafael Estrada <rafaelestrada@comcast.net>
2020-12-13 21:53:14 -0800

    first commit

---
+++ src/Pages/Projects/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import{white} from '@carbon/colors';
+
+export const ProjectItem=styled.li`
+margin-top:1rem;
+padding-bottom:1rem;
+border-bottom:1px solid ${white};
+`
+
+export const ProjectTitle = styled.div`
+margin-top:1.2rem;
+`
+
