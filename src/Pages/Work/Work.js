87ddb77c28951d61ac1526717892975a28c6fec2
Rafael Estrada <rafaelestrada@comcast.net>
2020-12-20 22:25:41 -0800

    fixed work folder location

--- src/Pages/Work/Work.js
+++ src/Pages/Work/Work.js
@@ -14,7 +14,7 @@ const Work = ({ user }) => {
               <WorkTitle>{work.position}</WorkTitle>
               <div>
                 <JobTitle>{work.company}</JobTitle> <span>{work.location}</span>
-                <span> %sdot;</span>
+                <span> &sdot;</span>
                 <span>
                   {work.start.year} to {work.end.year}
                 </span>
